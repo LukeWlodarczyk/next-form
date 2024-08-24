ef0f7cc87cd166c9e67ba636533b8e1f0ca642e8
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-08-24 19:40:45 +0200

    add placeholders

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -11,9 +11,24 @@ export default function Form() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form action={action} className="flex flex-col items-center">
-        <input className="mb-4 p-2 rounded" type="text" name="fullName" />
-        <input className="mb-4 p-2 rounded" type="email" name="email" />
-        <input className="mb-4 p-2 rounded" type="phone" name="phone" />
+        <input
+          className="mb-4 p-2 rounded"
+          type="text"
+          name="fullName"
+          placeholder="Maja Seger"
+        />
+        <input
+          className="mb-4 p-2 rounded"
+          type="email"
+          name="email"
+          placeholder="majaseger@gmail.com"
+        />
+        <input
+          className="mb-4 p-2 rounded"
+          type="tel"
+          name="phone"
+          placeholder="999999999"
+        />
         <SubmitFormButton>Submit</SubmitFormButton>
         {Boolean(error) && <p className="text-rose-600 mt-4">{error}</p>}
       </form>
